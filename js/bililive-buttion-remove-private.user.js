f6fac62ddde93a6695dba7c5697b3c95da071be6
zl <zl@bestarschool.com>
2019-06-19 22:38:12 +0800

    批量移除

--- js/bililive-buttion-remove-private.user.js
+++ js/bililive-buttion-remove-private.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili直播-移除增加亲密度确认按钮
 // @namespace    http://pdkst.github.io/
-// @version      0.3
+// @version      0.1
 // @description  当前直播间有舰长上舰时，增加亲密度时需要来回确认，十分不便，这个脚本将移除确认按钮的显示，免去确认的痛苦，快速连续获得亲密度；第一次时仍然会提示，但是不用管继续点，之后就不会出现了
 // @author       pdkst
 // @supportURL   https://github.com/pdkst/MonkeyScript/issues
@@ -21,7 +21,7 @@
     clickArea();
     function clickArea(){
         //下方区域
-        //$('#chat-popup-area-vm > div > div > div.main div').click();
+        $('#chat-popup-area-vm > div > div > div.main div').click();
         //抽奖确认按钮
         $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div.popup-btn-ctnr.t-center > button').click();
         //其他确认按钮
@@ -30,11 +30,14 @@
     }
     //点击时移除相关礼物消息
     $(document).on('click', '#chat-history-list > div.chat-item.system-msg.border-box > div > a', function(){
-        var href = $(this).attr('href');
-        console.log('href = ' + href);
-        $('#chat-history-list > div > div > a').filter(function(i,e){
-            return $(e).attr('href') == href
-        }).each(function(i, e){
+        var text = $(this).select('span:nth-child(4)').text();
+        console.log('text = ' + text);
+        var all = $('#chat-history-list > div > div > a');
+        var filtered = all.filter(function(i, e){
+            return $(e).select('span:nth-child(4)').text() == text
+        });
+        console.log('剩余总共' + all.length + '个中的' + filtered.length + '个被移除了。');
+        filtered.each(function(i, e){
             $(e).parent().parent().remove();
         });
     });
