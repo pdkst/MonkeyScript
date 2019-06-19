bc38e75ff83bf529fa780e0088c27340a9cd522a
zl <zl@bestarschool.com>
2019-06-19 21:52:06 +0800

    添加移除重复礼物消息（beta）

--- js/bililive-buttion-remove-private.user.js
+++ js/bililive-buttion-remove-private.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili直播-移除增加亲密度确认按钮
 // @namespace    http://pdkst.github.io/
-// @version      0.2
+// @version      0.3
 // @description  当前直播间有舰长上舰时，增加亲密度时需要来回确认，十分不便，这个脚本将移除确认按钮的显示，免去确认的痛苦，快速连续获得亲密度；第一次时仍然会提示，但是不用管继续点，之后就不会出现了
 // @author       pdkst
 // @supportURL   https://github.com/pdkst/MonkeyScript/issues
@@ -13,10 +13,12 @@
 (function() {
     'use strict';
     $(document).on('click', "body > div.link-popup-ctnr > div > div.body-merge.w-100.h-100.p-absolute.p-zero", function(e){
+        //抽奖确认按钮
         $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div.popup-btn-ctnr.t-center > button').click();
+        //其他确认按钮
+        $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div > div > button').click();
     });
-    //setInterval(clickArea, 100 + Math.random() * 150);
-
+    clickArea();
     function clickArea(){
         //下方区域
         $('#chat-popup-area-vm > div > div > div.main div').click();
@@ -24,5 +26,16 @@
         $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div.popup-btn-ctnr.t-center > button').click();
         //其他确认按钮
         $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div > div > button').click();
+        setTimeout(clickArea, 400 + Math.random() * 150);
     }
+    //点击时移除相关礼物消息
+    $(document).on('click', '#chat-history-list > div.chat-item.system-msg.border-box > div > a', function(){
+        var href = $(this).attr('href');
+        console.log('href = ' + href);
+        $('#chat-history-list > div > div > a').filter(function(i,e){
+            return $(e).attr('href') == href
+        }).each(function(i, e){
+            $(e).parent().parent().remove();
+        });
+    });
 })();
