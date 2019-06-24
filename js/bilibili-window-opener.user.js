f9dd65201c04371f94ef1be0611c7c826c51ed3f
zl <zl@bestarschool.com>
2019-06-24 14:05:07 +0800

    update & bugfix

--- js/bilibili-window-opener.user.js
+++ js/bilibili-window-opener.user.js
@@ -1,22 +1,22 @@
 // ==UserScript==
 // @name         自动打开礼物（beta）
 // @namespace    http://pdkst.github.io/
-// @version      0.1
+// @version      0.2
 // @description  自动打开礼物（beta）
 // @author       pdkst
 // @match        *://live.bilibili.com/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function($) {
     'use strict';
     var srcArr = ['/3822389', '/21304638'];
-    if(window.opener && srcArr.includes(window.opener.window.location.pathname)){
-        //被打开的窗口5秒后关闭
-        setTimeout(window.close, 5000);
-    }else{
+    if (srcArr.includes(window.location.pathname)){
         //循环打开礼物窗口
         setInterval(circleFunction, 1000);
+    } else if(window.opener && srcArr.includes(window.opener.window.location.pathname)){
+        //被打开的窗口5秒后关闭
+        setTimeout(window.close, 10000);
     }
 
     function circleFunction(){
@@ -29,10 +29,10 @@
 
                 var $e = $(e);
                 var href = $e.attr('href');
-                console.log('href = ' + href);
+                //console.log('href = ' + href);
                 $e.click();
                 window.open(href, '_blank');
             });
         }
     }
-})();
+})(window.$ || window.jQuery);
