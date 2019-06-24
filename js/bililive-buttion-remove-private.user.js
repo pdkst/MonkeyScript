a2a593f28aa557ca63eb71dbaa84be7cdc791f77
zl <zl@bestarschool.com>
2019-06-24 14:05:07 +0800

    update & bugfix

--- js/bililive-buttion-remove-private.user.js
+++ js/bililive-buttion-remove-private.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Bilibili直播-移除增加亲密度确认按钮
 // @namespace    http://pdkst.github.io/
-// @version      0.4
+// @version      0.5
 // @description  当前直播间有舰长上舰时，增加亲密度时需要来回确认，十分不便，这个脚本将移除确认按钮的显示，免去确认的痛苦，快速连续获得亲密度；第一次时仍然会提示，但是不用管继续点，之后就不会出现了
 // @author       pdkst
 // @supportURL   https://github.com/pdkst/MonkeyScript/issues
 // @match        https://live.bilibili.com/*
-// @require      https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js
+// @require      https://static.hdslb.com/live-static/libs/jquery/jquery-1.11.3.min.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function($) {
     'use strict';
     $(document).on('click', "body > div.link-popup-ctnr > div > div.body-merge.w-100.h-100.p-absolute.p-zero", function(e){
         //抽奖确认按钮
@@ -46,4 +46,4 @@
             $(e).parent().parent().remove();
         });
     });
-})();
+})(window.$ || window.jQuery);
