1a7a3e104601e521c39747086eee84301d70a0db
pdkst <pdkstudio@163.com>
2019-06-27 16:55:35 +0800

    format code

--- js/bilibili-window-opener.user.js
+++ js/bilibili-window-opener.user.js
@@ -5,33 +5,35 @@
 // @description  自动打开礼物（beta）
 // @author       pdkst
 // @match        *://live.bilibili.com/*
+// @require      https://static.hdslb.com/live-static/libs/jquery/jquery-1.11.3.min.js
 // @grant        none
 // ==/UserScript==
 
-(function($) {
+(function ($) {
     'use strict';
     var srcArr = ['/3822389', '/21304638'];
-    if (srcArr.includes(window.location.pathname)){
-        //循环打开礼物窗口
-        setInterval(circleFunction, 1000);
-    } else if(window.opener && srcArr.includes(window.opener.window.location.pathname)){
-        //被打开的窗口10秒后关闭
-        setTimeout(window.close, 10000);
-    }
 
-    function circleFunction(){
+    function circleFunction() {
         var giftLinks = $('#chat-history-list > div.chat-item.system-msg.border-box > div > a');
         console.log('礼物总数：' + giftLinks.length);
-        if(giftLinks.length){
-            giftLinks.filter(function (i, e) { 
-                return i == 0;
-            }).each(function (i, e) { 
-                var $e = $(e);
-                var href = $e.attr('href');
+        if (giftLinks.length) {
+            giftLinks.filter(function (i) {
+                return i === 0;
+            }).each(function (i, e) {
+                var $e = $(e), href = $e.attr('href');
                 //console.log('href = ' + href);
                 $e.click();
                 window.open(href, '_blank');
             });
         }
     }
+
+    if (srcArr.includes(window.location.pathname)) {
+        //循环打开礼物窗口
+        setInterval(circleFunction, 1000);
+    } else if (window.opener && srcArr.includes(window.opener.window.location.pathname)) {
+        //被打开的窗口10秒后关闭
+        setTimeout(window.close, 10000);
+    }
+
 })(window.$ || window.jQuery);
