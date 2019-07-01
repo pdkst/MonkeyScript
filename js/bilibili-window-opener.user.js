c0980bdb38bdc968e112b44fc317400ea2565a75
pdkst <pdkstudio@163.com>
2019-07-01 10:17:30 +0800

    add history

--- js/bilibili-window-opener.user.js
+++ js/bilibili-window-opener.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动打开礼物（beta）
 // @namespace    http://pdkst.github.io/
-// @version      0.5
+// @version      0.6
 // @description  自动打开礼物（beta）
 // @author       pdkst
 // @match        *://live.bilibili.com/*
@@ -12,8 +12,9 @@
 (function ($) {
     'use strict';
     var srcArr = [];
-    srcArr.push('/3822389');//角龙
-    srcArr.push('/21304638');//狗妈
+    var openHistory = [];
+    srcArr.push('/3822389'); //角龙
+    srcArr.push('/21304638'); //狗妈
 
     function circleFunction() {
         var giftLinks = $('#chat-history-list > div.chat-item.system-msg.border-box > div > a');
@@ -26,7 +27,34 @@
                 //var href = $e.attr('href');
                 //console.log('href = ' + href);
                 //window.open(href, '_blank');
+                var text = $e.children('span:nth-child(4)').text();
+
+                if(openHistory.includes(text)){
+                    //7秒内已打开
+                    console.log("includes = " + shift);
+                    return;
+                }else{
+                    //未打开放入历史
+                    openHistory.push(text);
+                    //仅保存7条
+                    if(openHistory.length > 7){
+                        var shift = openHistory.shift();
+                        console.log("shift = " + shift);
+                    }
+                }
+
+                //打开逻辑
                 $e.children(':first').click();
+                //移除当前消息框
+                console.log('open = ' + text);
+                var all = $('#chat-history-list > div > div > a');
+                var filtered = all.filter(function (i, e) {
+                    return $(e).children('span:nth-child(4)').text() == text
+                });
+                console.log('剩余总共' + all.length + '个中的' + filtered.length + '个被移除了。');
+                filtered.each(function (i, e) {
+                    $(e).parent().parent().remove();
+                });
             });
         }
     }
