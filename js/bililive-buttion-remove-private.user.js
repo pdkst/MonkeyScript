a1dd5a39dd0d1080818a68aab1ead4fb43fdaa92
pdkst <zl@bestarschool.com>
2019-08-03 23:49:01 +0800

    log for present

--- js/bililive-buttion-remove-private.user.js
+++ js/bililive-buttion-remove-private.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili直播-移除增加亲密度确认按钮
 // @namespace    http://pdkst.github.io/
-// @version      0.6
+// @version      0.7
 // @description  当前直播间有舰长上舰时，增加亲密度时需要来回确认，十分不便，这个脚本将移除确认按钮的显示，免去确认的痛苦，快速连续获得亲密度；第一次时仍然会提示，但是不用管继续点，之后就不会出现了
 // @author       pdkst
 // @supportURL   https://github.com/pdkst/MonkeyScript/issues
@@ -13,30 +13,59 @@
 
 (function ($) {
     'use strict';
-    $(document).on('click', "body > div.link-popup-ctnr > div > div.body-merge.w-100.h-100.p-absolute.p-zero", closePopupWin);
 
+    //=======方法区========
     /**
      * 关闭窗口点击的确认按钮
      */
     function closePopupWin() {
         //抽奖确认
         var $button = $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div.popup-btn-ctnr.t-center > button');
-        //弹幕风暴输入框 body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div > div:nth-child(2) > input
-        if ($button.parent('div.popup-content-ctnr').children('div > div:nth-child(2) > input').length == 0) {
-            $button.click();
+        //排除包含弹幕风暴输入框的弹窗 body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div > div:nth-child(2) > input
+        if ($button.length){
+            console.log("$button = " + $button.length);
+            if($button.parent('div.popup-content-ctnr').children('div > div:nth-child(2) > input').length == 0) {
+                $button.click();
+            }
         }
+        var $otherButton = $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div > div > button');
         //其他确认按钮
-        $('body > div.link-popup-ctnr > div > div.dp-table-cell.v-middle > div > div.popup-content-ctnr > div > div > button').click();
+        if ($otherButton.length) {
+            console.log("$otherButton = " + $otherButton.length);
+            $otherButton.click();
+        }
     }
-    
+
+    //礼物区
+    function closePresentWin() {
+        var $presentArea = $('#chat-popup-area-vm > div > div > div.main');
+        if($presentArea.length){
+            //console.log("$presentArea = " + $presentArea.length);
+            $presentArea.each(function(i,e) {
+                var $presentAreaTitle = $(e).siblings("div.title");
+                //console.log("$presentAreaTitle = " + $presentAreaTitle.length + ' text = '+ $presentAreaTitle.text() + ' index = ' + ($presentAreaTitle.text().indexOf('已抽奖， 等待开奖') < 0));
+                if ($presentAreaTitle.length && $presentAreaTitle.text() && $presentAreaTitle.text().indexOf('已抽奖， 等待开奖') < 0) {
+                    console.log('text = '+ $presentAreaTitle.text() + ' index = ' + ($presentAreaTitle.text().indexOf('已抽奖， 等待开奖') < 0));
+                    //$(e).children("div").click();
+                    $(e).click();
+                }
+            });
+        }
+    }
+
+    //=======定时========
     setInterval(closePopupWin, 200);
 
     function clickArea() {
         //抽奖
-        $('#chat-popup-area-vm > div > div > div.main div').click();
+        closePresentWin();
+        //定时器循环
         setTimeout(clickArea, 200 + Math.random() * 150);
     }
 
+    //=======事件=========
+    //遮罩点击事件
+    $(document).on('click', "body > div.link-popup-ctnr > div > div.body-merge.w-100.h-100.p-absolute.p-zero", closePopupWin);
     //点击时移除相关礼物消息
     $(document).on('click', '#chat-history-list > div.chat-item.system-msg.border-box > div > a', function () {
         var text = $(this).children('span:nth-child(4)').text();
