d0406c3657722c3ef0c96e4b8353fb27c5ac25eb
pdkst <zl@bestarschool.com>
2019-09-09 19:35:37 +0800

    update version

--- js/bilibili-window-opener.user.js
+++ js/bilibili-window-opener.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         自动打开礼物（beta）
 // @namespace    http://pdkst.github.io/
-// @version      1.1.0
+// @version      1.2.0
 // @description  在待机页面等待时自动打开关闭礼物页面，此脚本并不会领取礼物，只会自动打开需要领礼物的界面
 // @author       pdkst
 // @match        *://live.bilibili.com/*
