e39299d7230cff9d4ff20519599ca507853abb12
pdkst <zl@bestarschool.com>
2019-11-10 22:18:12 +0800

    add 狂暴粽子

--- js/bililive-vtuber-hello.user.js
+++ js/bililive-vtuber-hello.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         B站直播Vtuber问候语
 // @namespace    http://pdkst.github.io/
-// @version      0.4
+// @version      0.5
 // @description  显示配置的vtuber的问候语，每个vtuber都有独特的问候语，才不是不知道，只是打不出来~
 // @author       pdkst
 // @supportURL   https://github.com/pdkst/MonkeyScript/issues
@@ -63,20 +63,24 @@ class VtuberConfig {
         this.ext = ext || [];
     }
 }
-var global_vtuber_config;
+
 function GlobalVtuberConfig() {
-    if (!global_vtuber_config) {
-        var config = {};
-        config['/'] = new VtuberConfig('粉丝名', '❤', 'おはよ。', 'こにちは', 'こんばんは', 'お疲れ様', '❤♪', 'VTUBER问候语', '2019-10-6 22:04:14', ['测试输出']);
-        config['/3822389'] = new VtuberConfig('黑白狐', '🐾', 'mahamaha', 'mahamaha', 'mahamaha', 'mabamaba', '❤🐾♪', 'VTUBER问候语', '2019-8-17 21:26:13');
-        config['/14917277'] = new VtuberConfig('湊阿夸', '⚓', 'おはようござりました！', '', 'こんあくあ～', 'おつあくあ～', '❤⚓♪', 'VTUBER问候语', '2019-8-21 22:06:18');
-        config['/12770821'] = new VtuberConfig('小肉干', '✿', 'おはようござりました！', '', 'こんばんは', 'お疲れ様', '❤✿♪❀♪', 'VTUBER问候语', '2019-8-24 16:49:59');
-        config['/14052636'] = new VtuberConfig('Shiori', '🍄', 'おは堕天使*ଘ(੭*ˊᵕˋ)੭* ੈ🍄‧₊˚', '', 'ユメ堕つ', 'お疲れ様', '❤🍄♪', 'VTUBER问候语', '2019-9-7 23:04:08');
-        config['/14327465'] = new VtuberConfig('花园猫', '🍯', 'おはセレナ', '', 'こんセレナ！', 'お疲れ様', '❤🍯♪', 'VTUBER问候语', '2019-10-6 22:06:19');
-        config['/21320551'] = new VtuberConfig('乙民', '♍', 'おはZ〜', 'こにちはZ', 'こんばんZ~', 'おつZ~', '❤♪', 'VTUBER问候语', '2019-11-5 22:04:04');
-        global_vtuber_config = config;
+    var global_vtuber_config = window.global_vtuber_config || {};
+    if (global_vtuber_config && Object.getOwnPropertyNames(global_vtuber_config).length) {
+        return global_vtuber_config;
     }
-    return config;
+    const defaultExtendArray = ['不要走：行かないで'];
+    var config = {};
+    config["e/"] = defaultExtendArray;
+    config['/'] = new VtuberConfig('粉丝名', '❤', 'おはよ。', 'こにちは', 'こんばんは', 'お疲れ様', '❤♪', 'VTUBER问候语', '2019-10-6 22:04:14');
+    config['/3822389'] = new VtuberConfig('黑白狐', '🐾', 'mahamaha', 'mahamaha', 'mahamaha', 'mabamaba', '❤🐾♪', 'VTUBER问候语', '2019-8-17 21:26:13');
+    config['/14917277'] = new VtuberConfig('湊阿夸', '⚓', 'おはようござりました！', '', 'こんあくあ～', 'おつあくあ～', '❤⚓♪', 'VTUBER问候语', '2019-8-21 22:06:18');
+    config['/12770821'] = new VtuberConfig('小肉干', '✿', 'おはようござりました！', '', 'こんばんは', 'お疲れ様', '❤✿♪❀♪', 'VTUBER问候语', '2019-8-24 16:49:59');
+    config['/14052636'] = new VtuberConfig('Shiori', '🍄', 'おは堕天使*ଘ(੭*ˊᵕˋ)੭* ੈ🍄‧₊˚', '', 'ユメ堕つ', 'お疲れ様', '❤🍄♪', 'VTUBER问候语', '2019-9-7 23:04:08');
+    config['/14327465'] = new VtuberConfig('花园猫', '🍯', 'おはセレナ', '', 'こんセレナ！', 'お疲れ様', '❤🍯♪', 'VTUBER问候语', '2019-10-6 22:06:19');
+    config['/21320551'] = new VtuberConfig('乙民', '♍', 'おはZ〜', 'こにちはZ', 'こんばんZ~', 'おつZ~', '❤♪', 'VTUBER问候语', '2019-11-5 22:04:04');
+    config['/21545232'] = new VtuberConfig('绿粽子', '🦋', 'おはるし〜', 'こにちは', 'こんるしー', 'おつるし~', '❤♪', 'VTUBER问候语', '2019-11-10 21:53:15');
+    return window.global_vtuber_config = config;
 }
 
 class ModelCreator extends Output {
@@ -107,16 +111,20 @@ class ModelCreator extends Output {
         this.prefix.push('</p>');
 
         //输出content
+        var extendArray = [];
         if (config && config.ext && config.ext.length) {
-            this.prefix.push('<p class="content">');
-            this.prefix.push('<ul>');
-            let _prefix = this.prefix;
-            config.ext.forEach(function (value, _index, array) {
-                _prefix.push('<li>' + (value || '') + '</li>');
-            })
-            this.prefix.push('</ul>');
-            this.prefix.push('</p>');
+            extendArray = config.ext;
+        } else {
+            extendArray = GlobalVtuberConfig()["e/"];
         }
+        this.prefix.push('<p class="content">');
+        this.prefix.push('<ul>');
+        let _prefix = this.prefix;
+        extendArray.forEach(function (value, _index, array) {
+            _prefix.push('<li>' + (value || '') + '</li>');
+        })
+        this.prefix.push('</ul>');
+        this.prefix.push('</p>');
     }
 
     output() {
