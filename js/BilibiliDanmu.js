7d1c02f5720d46d40c4604cd474bc41481dda238
pdkst <zl@bestarschool.com>
2020-08-04 23:23:56 +0800

    add DanmuConfig/BilibiliDanmuUtil

--- js/BilibiliDanmu.js
+++ js/BilibiliDanmu.js
@@ -1,3 +1,4 @@
+//see https://greasyfork.org/zh-CN/scripts/27759-bilibili-html5-live
 class RoomInit {
     id;
 
@@ -6,31 +7,63 @@ class RoomInit {
     }
 
     get(callback) {
-        $.get('https://api.live.bilibili.com/room/v1/Room/room_init', this)
+        this.getAsync()
             .success(callback)
             .fail(function (data) {
                 console.log(data);
             });
     }
 
-    getAsync() {
+    async getAsync() {
         return $.get('https://api.live.bilibili.com/room/v1/Room/room_init', this);
+    }
+
+    async getConfig() {
+        let roomResponse = await this.getAsync();
+
+    }
+}
+
+class DanmuConfig {
+    room_id;
+    platform = "pc";
+    player = "web";
+
+    constructor(roomId) {
+        this.room_id = roomId || 1;
+    }
+
+    async getAsync() {
+        return $.get('https://api.live.bilibili.com/room/v1/Danmu/getConf', this);
+    }
+
+    async getConfig() {
+        let response = await this.getAsync();
+        let data = response.data || {};
+        let list = data.host_server_list || [];
+        let server = list[0] || {};
+        return {
+            domain: server.host || 'broadcastlv.chat.bilibili.com',
+            ws: server.ws_port || 2243,
+            wss: server.wss_port || 2244,
+            port: server.port || 2243,
+            token: data.token,
+        };
     }
 }
 
 class BilibiliDanmu {
     roomId;
-    protocol = "ws";
+    uid;
+    protocol = "wss";
     domain;
+    port;
+    token;
 
     ws;
     status = "init";
 
-    portSetting = {
-        'ws': 7170,
-        'wss': 7172
-    }
-    packageSettings = {
+    static packageSettings = {
         rawHeaderLen: 16,
         packetOffset: 0,
         headerOffset: 4,
@@ -42,38 +75,50 @@ class BilibiliDanmu {
     textDecoder = this.getDecoder(true);
     textEncoder = this.getEncoder();
     heartbeatInterval;
+    ;
 
     constructor(roomId, config = {}) {
         this.roomId = roomId;
-        this.protocol = config.protocol || "ws";
-        this.domain = config.domain || "";
+        this.protocol = config.protocol || "wss";
+        this.domain = config.domain;
+        this.port = config.wss;
+        this.token = config.token;
+        console.log(this);
     }
 
-    connect() {
-        let port = this.portSetting[this.protocol];
-        this.ws = new WebSocket(this.protocol + '://' + this.domain + ':' + port + '/sub');
-        this.ws.send()
+    async connect() {
+        this.ws = new WebSocket('wss://' + this.domain + ':' + this.port + '/sub');
+        this.ws.onopen = this.onOpen.bind(this);
+        this.ws.onmessage = this.onMessage.bind(this);
+        this.ws.onclose = this.close.bind(this);
     }
 
-    onOpen() {
+    onOpen(e) {
         this.status = 'open';
         console.log(`${name}连接成功`, e);
+        const token = JSON.stringify({
+            'uid': this.uid || 0,
+            'roomid': this.roomId,
+            'token': this.token
+        });
+        const bodyBuf = this.textEncoder.encode(token);
+        this.sendBuffer(bodyBuf);
     }
 
     onMessage(event) {
         const data = event.data;
         const dataView = new DataView(data, 0);
-        var packetLen = dataView.getUint32(this.packageSettings.packetOffset);
+        var packetLen = dataView.getUint32(BilibiliDanmu.packageSettings.packetOffset);
         if (dataView.byteLength >= packetLen) {
-            var headerLen = dataView.getInt16(this.packageSettings.headerOffset);
-            var ver = dataView.getInt16(this.packageSettings.versionOffset);
-            var op = dataView.getUint32(this.packageSettings.operatorOffset);
-            var seq = dataView.getUint32(this.packageSettings.seqOffset);
+            var headerLen = dataView.getInt16(BilibiliDanmu.packageSettings.headerOffset);
+            var ver = dataView.getInt16(BilibiliDanmu.packageSettings.versionOffset);
+            var op = dataView.getUint32(BilibiliDanmu.packageSettings.operatorOffset);
+            var seq = dataView.getUint32(BilibiliDanmu.packageSettings.seqOffset);
             switch (op) {
                 case 8:
                     this.heartBeat();
                     if (!this.heartbeatInterval) {
-                        this.heartbeatInterval = setInterval(this.heartBeat, 30 * 1000);
+                        this.heartbeatInterval = setInterval(this.heartBeat.bind(this), 30 * 1000);
                     }
                     break;
                 case 3:
@@ -85,7 +130,7 @@ class BilibiliDanmu {
                     var msgBody;
                     for (var offset = 0; offset < msg.byteLength; offset += packetLen) {
                         packetLen = packetView.getUint32(offset);
-                        headerLen = packetView.getInt16(offset + this.packageSettings.headerOffset);
+                        headerLen = packetView.getInt16(offset + BilibiliDanmu.packageSettings.headerOffset);
                         if (ver === 2) {
                             // deflated message
                             const inflate = new Zlib.Inflate(new Uint8Array(msg.slice(offset + headerLen, offset + packetLen)), {resize: true});
@@ -111,13 +156,13 @@ class BilibiliDanmu {
 
     sendBuffer(bodyBuffer) {
         const bodyLength = bodyBuffer && bodyBuffer.length || 0
-        const headerBuf = new ArrayBuffer(this.packageSettings.rawHeaderLen);
+        const headerBuf = new ArrayBuffer(BilibiliDanmu.packageSettings.rawHeaderLen);
         const headerView = new DataView(headerBuf, 0);
-        headerView.setInt32(this.packageSettings.packetOffset, this.packageSettings.rawHeaderLen + bodyLength);
-        headerView.setInt16(this.packageSettings.headerOffset, this.packageSettings.rawHeaderLen);
-        headerView.setInt16(this.packageSettings.versionOffset, 1);
-        headerView.setInt32(this.packageSettings.operatorOffset, 2);
-        headerView.setInt32(this.packageSettings.seqOffset, 1);
+        headerView.setInt32(BilibiliDanmu.packageSettings.packetOffset, BilibiliDanmu.packageSettings.rawHeaderLen + bodyLength);
+        headerView.setInt16(BilibiliDanmu.packageSettings.headerOffset, BilibiliDanmu.packageSettings.rawHeaderLen);
+        headerView.setInt16(BilibiliDanmu.packageSettings.versionOffset, 1);
+        headerView.setInt32(BilibiliDanmu.packageSettings.operatorOffset, 2);
+        headerView.setInt32(BilibiliDanmu.packageSettings.seqOffset, 1);
         if (bodyBuffer) {
             this.ws.send(this.mergeArrayBuffer(headerBuf, bodyBuffer));
         } else {
@@ -138,10 +183,11 @@ class BilibiliDanmu {
         this.status = "close"
         this.ws.close();
         console.log('close');
+        clearInterval(this.heartbeatInterval);
     }
 
     getEncoder() {
-        const unsafeWindow = unsafeWindow || window;
+        const unsafeWindow = window.unsafeWindow || window;
         if (unsafeWindow['TextEncoder']) {
             return new unsafeWindow['TextEncoder']();
         } else {
@@ -159,7 +205,7 @@ class BilibiliDanmu {
     }
 
     getDecoder(isUseful) {
-        const unsafeWindow = unsafeWindow || window;
+        const unsafeWindow = window.unsafeWindow || window;
         if (unsafeWindow['TextDecoder'] && isUseful) {
             return new unsafeWindow['TextDecoder']();
         } else {
@@ -169,5 +215,15 @@ class BilibiliDanmu {
                 }
             };
         }
+    }
+}
+
+class BilibiliDanmuUtil {
+    static async init() {
+        let w = window.unsafeWindow || window;
+        let roomId = w.BilibiliLive.ROOMID;
+        let config = await new DanmuConfig(roomId).getConfig();
+        let danmu = new BilibiliDanmu(roomId, config);
+        await danmu.connect();
     }
 }
