dc1e463349d146ac134de2a0ec2f2c47c3667d78
pdkst <zl@bestarschool.com>
2020-08-07 23:42:40 +0800

    add Operation、refactor

--- js/BilibiliDanmu.js
+++ js/BilibiliDanmu.js
@@ -20,8 +20,7 @@ class RoomInit {
 
     async getConfig() {
         let roomResponse = await this.getAsync();
-
-
+        return roomResponse.data.room_id;
     }
 }
 
@@ -52,15 +51,48 @@ class DanmuConfig {
         };
     }
 }
+// go-common\app\service\main\broadcast\model\operation.go
+// https://github.com/xfgryujk/blivedm/blob/master/blivedm.py
+// https://github.com/lovelyyoshino/Bilibili-Live-API/blob/master/API.WebSocket.md
+class Operation {
+    static HANDSHAKE = 0
+    static HANDSHAKE_REPLY = 1
+    static HEARTBEAT = 2
+    static HEARTBEAT_REPLY = 3
+    static SEND_MSG = 4
+    static SEND_MSG_REPLY = 5
+    static DISCONNECT_REPLY = 6
+    static AUTH = 7
+    static AUTH_REPLY = 8
+    static RAW = 9
+    static PROTO_READY = 10
+    static PROTO_FINISH = 11
+    static CHANGE_ROOM = 12
+    static CHANGE_ROOM_REPLY = 13
+    static REGISTER = 14
+    static REGISTER_REPLY = 15
+    static UNREGISTER = 16
+    static UNREGISTER_REPLY = 17
+    //  B站业务自定义OP
+    //  MinBusinessOp = 1000
+    //  MaxBusinessOp = 10000
+}
 
 class DanmuPack {
+    // arraybuffer
     data;
+    offset;
     dataView;
     packetLength;
     headerLength;
     version;
     operator;
     sequence;
+    // arraybuffer
+    value;
+
+    static textDecoder = DanmuPack.getDecoder(true);
+    static textEncoder = DanmuPack.getEncoder();
 
     static packageSettings = {
         rawHeaderLen: 16,
@@ -71,16 +103,68 @@ class DanmuPack {
         seqOffset: 12,
     }
 
-    constructor(data) {
+    constructor(data, offset = 0) {
         this.data = data;
-        this.dataView = new DataView(data, 0);
+        this.offset = offset = offset || 0;
+        this.dataView = new DataView(data, offset);
         this.packetLength = this.dataView.getUint32(DanmuPack.packageSettings.packetOffset)
         this.headerLength = this.dataView.getInt16(DanmuPack.packageSettings.headerOffset);
         this.version = this.dataView.getInt16(DanmuPack.packageSettings.versionOffset);
         this.operator = this.dataView.getUint32(DanmuPack.packageSettings.operatorOffset);
         this.sequence = this.dataView.getUint32(DanmuPack.packageSettings.seqOffset);
+        this.value = data.slice(this.offset + this.headerLength, this.offset + this.packetLength);
     }
 
+    body() {
+        let msgBody = DanmuPack.textDecoder.decode(this.value);
+        if (!msgBody) {
+            const textDecoder = DanmuPack.getDecoder(false);
+            msgBody = textDecoder.decode(this.value);
+        }
+        return msgBody;
+    }
+
+    bodyAsJson() {
+        return JSON.parse(this.body());
+    }
+
+    hasNext() {
+        return this.offset + this.packetLength < this.data.byteLength;
+    }
+
+    next() {
+        let nextPackOffset = this.offset + this.packetLength;
+        return new DanmuPack(this.data, nextPackOffset);
+    }
+
+    zlibInflate() {
+        const inflate = new Zlib.Inflate(new Uint8Array(this.value), { resize: true });
+        const inflatedMsg = inflate.decompress();
+        return new DanmuPack(inflatedMsg.buffer);
+    }
+
+    pakoInflate() {
+        const inflate = pako.inflate(new Uint8Array(this.value));
+        // debugger;
+        return new DanmuPack(inflate.buffer);
+    }
+
+    /**
+     * 
+     * @param {String} str 
+     */
+    static packString(str) {
+        if (!str) {
+            return DanmuPack.packBuffer();
+        }
+        let bodyBuffer = DanmuPack.textEncoder.encode(str);
+        return DanmuPack.packBuffer(bodyBuffer);
+    }
+
+    /**
+     * 
+     * @param {ArrayBuffer} bodyBuffer 
+     */
     static packBuffer(bodyBuffer) {
         const bodyLength = bodyBuffer && bodyBuffer.length || 0
         const operator = bodyBuffer && bodyBuffer.length && 7 || 2
@@ -98,7 +182,11 @@ class DanmuPack {
         console.log("header = ", headerBuf)
         return DanmuPack.mergeArrayBuffer(headerBuf, bodyBuffer);
     }
-
+    /**
+     * 
+     * @param {ArrayBuffer} ab1 
+     * @param {ArrayBuffer} ab2 
+     */
     static mergeArrayBuffer(ab1, ab2) {
         const u81 = new Uint8Array(ab1),
             u82 = new Uint8Array(ab2),
@@ -107,8 +195,43 @@ class DanmuPack {
         res.set(u82, ab1.byteLength);
         return res.buffer;
     }
-}
 
+    static getEncoder() {
+        const unsafeWindow = window.unsafeWindow || window;
+        if (unsafeWindow['TextEncoder']) {
+            return new unsafeWindow['TextEncoder']('utf-8');
+        } else {
+            return {
+                encode: function (str) {
+                    const buf = new ArrayBuffer(str.length);
+                    const bufView = new Uint8Array(buf);
+                    for (let i = 0; i < str.length; i++) {
+                        bufView[i] = str.charCodeAt(i);
+                    }
+                    return bufView;
+                }
+            };
+        }
+    }
+
+    /**
+     * 
+     * @param {Boolean} isUseful 
+     */
+    static getDecoder(isUseful) {
+        const unsafeWindow = window.unsafeWindow || window;
+        if (unsafeWindow['TextDecoder'] && isUseful) {
+            return new unsafeWindow['TextDecoder']('utf-8');
+        } else {
+            return {
+                decode: function (buf) {
+                    let fromCharString = String.fromCharCode.apply(null, new Uint8Array(buf));
+                    return decodeURIComponent(unsafeWindow.escape(fromCharString));
+                }
+            };
+        }
+    }
+}
 class BilibiliDanmu {
     roomId;
     uid;
@@ -120,8 +243,6 @@ class BilibiliDanmu {
     ws;
     status = "init";
 
-    textDecoder = this.getDecoder(true);
-    textEncoder = this.getEncoder();
     heartbeatInterval;
     listener;
 
@@ -143,77 +264,91 @@ class BilibiliDanmu {
         this.ws.onerror = this.onError.bind(this);
     }
 
+    /**
+     * 连接打开时
+     * @param {MessageEvent} e 
+     */
     onOpen(e) {
         this.status = 'open';
         console.log(`${name}连接成功`, e);
-        const token = JSON.stringify({
+        const firstConfig = JSON.stringify({
             'uid': this.uid || 0,
             'roomid': this.roomId,
             'token': this.token
         });
-        const bodyBuf = this.textEncoder.encode(token);
-        this.sendBuffer(bodyBuf);
+        this.send(firstConfig);
     }
-
+    /**
+     * 绑定websocket消息处理方法
+     * @param {MessageEvent} event 
+     */
     async onMessage(event) {
         const data = event.data;
-        console.log(event);
-        debugger;
+        //console.log(event);
         window.devent = event;
         let danmuPack = new DanmuPack(await data.arrayBuffer());
         this.messageHandler(danmuPack);
     }
-
+    /**
+     * 处理弹幕逻辑
+     * @param {DanmuPack} danmuPack 
+     */
     messageHandler(danmuPack) {
         let dataView = danmuPack.dataView;
         let packetLen = danmuPack.packetLength;
         if (dataView.byteLength >= packetLen) {
+            debugger;
             switch (danmuPack.operator) {
-                case 8:
+                case Operation.AUTH_REPLY:
                     this.heartBeat();
                     if (!this.heartbeatInterval) {
                         this.heartbeatInterval = setInterval(this.heartBeat.bind(this), 30 * 1000);
                     }
                     break;
-                case 3:
-                    if (this.listener) this.listener('online', dataView.getInt32(16));
+                case Operation.HEARTBEAT_REPLY:
+                    if (this.listener) {
+                        this.listener('online', dataView.getInt32(16));
+                    }
                     break;
-                case 5:
-                    const msg = danmuPack.data;
-                    for (let offset = 0; offset < msg.byteLength; offset += packetLen) {
-                        packetLen = dataView.getUint32(offset);
-                        let headerLen = dataView.getInt16(offset + DanmuPack.packageSettings.headerOffset);
+                case Operation.SEND_MSG_REPLY:
+                    for (; ;) {
                         if (danmuPack.version === 2) {
                             // deflated message
-                            const inflate = new Zlib.Inflate(new Uint8Array(msg.slice(offset + headerLen, offset + packetLen)), {resize: true});
-                            const inflatedMsg = inflate.decompress();
-                            this.ws.onmessage({data: inflatedMsg.buffer});
-                        } else if (packetLen) {
-                            let msgBody = this.textDecoder.decode(msg.slice(offset + headerLen, offset + packetLen));
-                            if (!msgBody) {
-                                const textDecoder = this.getDecoder(false);
-                                msgBody = textDecoder.decode(msg.slice(offset + headerLen, offset + packetLen));
-                            }
+                            console.log("inflated")
+                            //this.messageHandler(danmuPack.zlibInflate());
+                            this.messageHandler(danmuPack.pakoInflate());
+                        } else if (danmuPack.value.byteLength) {
                             if (this.listener) {
-                                this.listener('msg', msgBody);
+                                this.listener('msg', danmuPack.bodyAsJson());
                             }
                         }
+                        if (!danmuPack.hasNext()) {
+                            break;
+                        }
+                        danmuPack = danmuPack.next();
                     }
                     break;
             }
         }
     }
-
+    /**
+     * 发送心跳包（空包）
+     */
     heartBeat() {
-        this.sendBuffer();
+        this.send();
+    }
+    /**
+     * 主动发送
+     * @param {String} body 
+     */
+    send(body) {
+        const buffer = DanmuPack.packString(body);
+        this.ws.send(buffer);
     }
 
-    sendBuffer(bodyBuffer) {
-        const sendBuffer = DanmuPack.packBuffer(bodyBuffer);
-        this.ws.send(sendBuffer);
-    }
-
-
+    /**
+     * 连接关闭
+     */
     close() {
         this.status = "close"
         this.ws.close();
@@ -225,51 +360,25 @@ class BilibiliDanmu {
         console.log('onError:', e)
     }
 
-    getEncoder() {
-        const unsafeWindow = window.unsafeWindow || window;
-        if (unsafeWindow['TextEncoder']) {
-            return new unsafeWindow['TextEncoder']();
-        } else {
-            return {
-                encode: function (str) {
-                    const buf = new ArrayBuffer(str.length);
-                    const bufView = new Uint8Array(buf);
-                    for (let i = 0; i < str.length; i++) {
-                        bufView[i] = str.charCodeAt(i);
-                    }
-                    return bufView;
-                }
-            };
-        }
-    }
 
-    getDecoder(isUseful) {
-        const unsafeWindow = window.unsafeWindow || window;
-        if (unsafeWindow['TextDecoder'] && isUseful) {
-            return new unsafeWindow['TextDecoder']();
-        } else {
-            return {
-                decode: function (buf) {
-                    let fromCharString = String.fromCharCode.apply(null, new Uint8Array(buf));
-                    return decodeURIComponent(unsafeWindow.escape(fromCharString));
-                }
-            };
-        }
-    }
 }
 
 class BilibiliDanmuUtil {
     static async init() {
+        if (!window.pako) {
+            await $.getScript('https://cdn.bootcdn.net/ajax/libs/pako/1.0.11/pako.min.js')
+        }
         let w = window.unsafeWindow || window;
         let roomId = w.BilibiliLive.ROOMID;
         let config = await new DanmuConfig(roomId).getConfig();
-        config.uid = w.BilibiliLive.UID;
+        config.uid = 21449083 || w.BilibiliLive.UID;
         window.danmu = new BilibiliDanmu(roomId, config);
         danmu.listener = (type, msg) => {
             if (type === "msg") {
-                console.log("msg = " + msg)
+                console.log("msg = ", msg)
             }
         }
         await danmu.connect();
     }
 }
+
