1592967fe6166fe1f8a58a0423372b0dea0f238c
pdkst <zl@bestarschool.com>
2021-07-15 23:17:23 +0800

    fix mix

--- js/qiqidongman.com.user.js
+++ js/qiqidongman.com.user.js
@@ -2,9 +2,10 @@
 // @name         奇奇动漫屏蔽弹窗
 // @namespace    http://pdkst.github.io/MonkeyScript/qiqidongman.com.user.js
 // @version      0.1
-// @description  屏蔽弹窗
+// @description  try to take over the world!
 // @author       You
 // @match        https://m.qiqidongman.com/v/*
+// @match        https://m.qiqidongman.com
 // @grant        none
 // ==/UserScript==
 
@@ -13,4 +14,6 @@
 	$(function () {
 		$('#area-deny-box').remove()
 	})
+	$('#area-deny-box').css('display', 'none')
+	document.querySelector('#area-deny-box').remove()
 })(window.$ || window.jQuery)
