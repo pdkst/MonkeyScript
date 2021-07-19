67a750eaf22878705a698f196fab9f12952f1752
pdkst <zl@bestarschool.com>
2021-07-19 23:05:46 +0800

    set timeout

--- js/qiqidongman.com.user.js
+++ js/qiqidongman.com.user.js
@@ -2,26 +2,20 @@
 // @name         奇奇动漫屏蔽弹窗
 // @namespace    http://pdkst.github.io/MonkeyScript/qiqidongman.com.user.js
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://m.qiqidongman.com/v/*
-// @match        https://m.qiqidongman.com
+// @description  移除区域检查弹框
+// @author       pdkst
+// @match        https://m.qiqidongman.com/*
 // @grant        none
 // ==/UserScript==
 
 ;(function ($) {
 	'use strict'
-	$(function () {
-		$('#area-deny-box').remove()
-	})
-	$('#area-deny-box').css('display', 'none')
-	document.querySelector('#area-deny-box')?.remove()
+	$(removeBlock)
 
 	function removeBlock() {
-		document.querySelector('#area-deny-box')?.remove()
+		const $denyBox = document.querySelector('#area-deny-box')
+		$denyBox?.remove()
+		$denyBox || setTimeout(removeBlock, 300)
 	}
-
 	setTimeout(removeBlock, 100)
-	setTimeout(removeBlock, 500)
-	setTimeout(removeBlock, 1500)
 })(window.$ || window.jQuery)
